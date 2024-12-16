79f7d49b993df15afaad05f2e30f6ad7e92cc818
Crumbed <kaiharrelson5466@gmail.com>
2024-12-16 14:09:10 -0500

    basic input

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+
+use std::io;
+use io::Write;
+
+pub const VERSION: &'static str = "v0.0.2";
+
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+    //let args = std::env::args() .collect::<Vec<String>>(); 
+
+    println!("Repl Environment {VERSION}");
+    let mut out = io::stdout();
+    let stdin = io::stdin();
+    'main : loop {
+        print!("> ");
+        out.flush()?;
+        stdin.read_line(&mut input)?;
+        if input == ".exit\n" { break 'main Ok(()); }
+
+        println!("eq: {input}");
+
+        input.clear();
+    }
+}
